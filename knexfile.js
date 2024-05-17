9aa56a49081c2b71b8da086ebaa93e396b7656c3
Pichau <pedroarthursehn@gmail.com>
2024-05-17 19:25:33 -0300

    fixing pool name

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    poll:{
+    pool:{
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
     },
     migrations: {
