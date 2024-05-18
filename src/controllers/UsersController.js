82567172759186a683d9da33794e32f53e92ef21
Pichau <pedroarthursehn@gmail.com>
2024-05-18 18:58:14 -0300

    add user authentication

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -17,22 +17,22 @@ class UsersController {
 
         return response.status(201).json();
     }
-    
+
     async update(request, response) {
-        const { name, email, password, old_password} = request.body
-        const {id} = request.params
+        const { name, email, password, old_password } = request.body
+        const user_id = request.user.id
 
         const database = await sqliteConnection()
-        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
-        
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
+
         if (!user) throw new AppError("Usuário não encontrado")
 
         const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
-        if (userWithUpdatedEmail &&  userWithUpdatedEmail.id != user.id) throw new AppError("Este e-mail já está em uso")
-        
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id != user.id) throw new AppError("Este e-mail já está em uso")
+
         user.name = name ?? user.name
         user.email = email ?? user.email
-        
+
         if (password && !old_password) throw new AppError("Você precisa informar sua senha antiga para definir a nova senha")
         if (password && old_password) {
             const checkOldPassword = await compare(old_password, user.password)
@@ -47,10 +47,10 @@ class UsersController {
             password = ?,
             updated_at = DATETIME('now')
             WHERE id = ?`,
-            [user.name, user.email, user.password, id]
-            )
+            [user.name, user.email, user.password, user_id]
+        )
 
-            return response.status(200).json()
+        return response.status(200).json()
     }
 }
 
